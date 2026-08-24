81c2ecd1ac1f8a6e285b9f85805107c5e84473dd
garakyanangelina-creator <garakyanangelina@gmail.com>
2026-08-24 20:28:57 +0400

    Revert header logo to PNG

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
       <div className="container-page flex h-18 items-center justify-between py-2">
         <Link href="/" className="flex items-center">
           <img
-            src="/logo.svg"
+            src="/logo-new.png"
             alt="Yerevan Real Estate"
             style={{ height: "72px", width: "auto" }}
           />
